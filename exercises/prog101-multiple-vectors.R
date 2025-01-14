f90ba347b787d35d7bb54d9a4afc583644d3ccda
Cassidy Creighton <cassidycreighton@ucsb.edu>
2025-01-14 08:05:19 -0800

    Prog101

--- exercises/prog101-multiple-vectors.R
+++ exercises/prog101-multiple-vectors.R
@@ -19,12 +19,13 @@ instrument_recovered_hm <- c(1600, 1920, 2015)
 instrument_recovered_h <- floor(instrument_recovered_hm / 100)
 #how many whole hours
 instrument_recovered_hdec <- (instrument_recovered_hm %% 100) / 60
+#hours
 instrument_recovered <- instrument_recovered_h + instrument_recovered_hdec
 
 instrument_durations <- instrument_recovered - instrument_deployed
 
 # Which site had the longest duration? Use conditional indexing.
-
+site <- c("Santa Cruz", "Santa Rosa", "San Miguel")
 site[which.max(instrument_durations)]
 
 site[max(instrument_durations) == instrument_durations]
